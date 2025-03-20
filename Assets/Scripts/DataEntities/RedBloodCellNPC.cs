b7fa78334f5938bbf2f423917a765bced73c15ab
Jing Qiang <tohjingqiang@gmail.com>
2025-03-20 17:07:55 +0800

    add RedBloodCellNPC manager skeleton (logic not implemented)

---
+++ Assets/Scripts/DataEntities/RedBloodCellNPC.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataEntities
+{
+    public class RedBloodCellNPC
+    {
+        public readonly static int numberOfOxygenCarried = 1;
+
+        public RedBloodCellNPC()
+        {
+            Debug.Log("Init RedBloodCellNPC");
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/RedBloodCellNPCsManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedBloodCellNPCsManager : MonoBehaviour
+{
+    public float currentRedBloodCellLevel;
+    public int numberOfRedBloodCellsNPCs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentRedBloodCellLevel = GlobalVariables.Instance.redBloodCellLevel.level;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newRedBloodCellLevel = GlobalVariables.Instance.redBloodCellLevel.level;
+        if (currentRedBloodCellLevel != newRedBloodCellLevel)
+        {
+            currentRedBloodCellLevel = newRedBloodCellLevel;
+            UpdateNumberOfRedBloodCellsNPCs();
+            UpdateRedBloodCellsNPCsGameUI();
+        }
+    }
+
+    private void UpdateNumberOfRedBloodCellsNPCs()
+    {
+        // Every 2 RBC level = 1 RBC NPC
+        numberOfRedBloodCellsNPCs = Mathf.FloorToInt(currentRedBloodCellLevel / 2);
+    }
+
+    public void UpdateRedBloodCellsNPCsGameUI()
+    {
+        // TODO: Update Game UI for RBC NPCs. If RBC falls belows a level, show less RBCs in the bloodstream
+        DataEntities.RedBloodCellNPC redBloodCellNPC = new();
+    }
+}
