0eb74f14337123703003b877a6d27d0677cb57b1
Lim Ding Wen <limdingwen@gmail.com>
2025-03-26 22:33:24 +0800

    Finish map

--- Assets/Scripts/DataEntities/PathSegment.cs
+++ Assets/Scripts/DataEntities/PathSegment.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class PathSegment : MonoBehaviour
 {
     public float radius = 5;
@@ -17,3 +21,23 @@ public class PathSegment : MonoBehaviour
         );
     }
 }
+
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(PathSegment))]
+public class PathSegmentEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        PathSegment pathSegment = (PathSegment)target;
+
+        if (GUILayout.Button("Reverse Path Segment"))
+        {
+            Undo.RecordObject(pathSegment.transform, "Reverse Path Segment");
+            pathSegment.transform.rotation = Quaternion.LookRotation(-pathSegment.transform.forward);
+            pathSegment.transform.position -= pathSegment.transform.forward * pathSegment.distance;
+        }
+    }
+}
+#endif
