8e18ab3b9a7951690bf5e2af9db51d6baa9b6d05
Jing Qiang <tohjingqiang@gmail.com>
2025-03-30 08:02:32 +0800

    bug fix

--- Assets/Scripts/Controllers/MainMenuController.cs
+++ Assets/Scripts/Controllers/MainMenuController.cs
@@ -7,21 +7,39 @@ public class MainMenuController : MonoBehaviour
 {
     public void StartEasyGameBtn()
     {
+        if (!canClick) return; // workaround for single click triggering twice
+        canClick = false;
+
         Debug.Log("Starting Easy Game");
         GlobalVariables.Instance.gameDifficulty = Enums.GameDifficulty.Easy;
         GlobalVariables.Instance.gameStarted = true;
         GlobalVariables.Instance.InitEasyDifficultyGameConfiguration();
 
         SceneManager.LoadScene("HumanModeScene");
+
+        // Reset after short delay; workaround for single click triggering twice
+        Invoke(nameof(ResetAddOxygenClick), 0.2f);
     }
 
     public void StartHardGameBtn()
     {
+        if (!canClick) return; // workaround for single click triggering twice
+        canClick = false;
+
         Debug.Log("Starting Hard Game");
         GlobalVariables.Instance.gameDifficulty = Enums.GameDifficulty.Hard;
         GlobalVariables.Instance.gameStarted = true;
         GlobalVariables.Instance.InitHardDifficultyGameConfiguration();
 
         SceneManager.LoadScene("HumanModeScene");
+
+        // Reset after short delay; workaround for single click triggering twice
+        Invoke(nameof(ResetAddOxygenClick), 0.2f);
+    }
+
+    private bool canClick = true; // workaround for single click triggering twice
+    private void ResetAddOxygenClick() // workaround for single click triggering twice
+    {
+        canClick = true;
     }
 }
